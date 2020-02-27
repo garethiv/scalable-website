569831496162f2e638af14cb4b35135227bba1b0
garethiv <gareth@garethveale.dev>
2020-02-27 08:36:05 +0000

    React changes & installed CORS

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  };
+
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { this.setState({ users: res.data }); })
+    .catch((err) => { console.log(err); });
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">Users</h1>
+              <hr/><br/>
+              {
+                this.state.users.map((user) => {
+                  return (
+                    <p
+                      key={user.id}
+                      className="box title is-4 username"
+                    >{ user.username }
+                    </p>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
 
 export default App;
