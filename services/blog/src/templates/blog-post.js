c3a047a755b96f88dda6471743ec1ca76ee41a33
garethiv <gareth@garethveale.dev>
2020-03-02 07:38:21 +0000

    prev and next buttons to post template

--- services/blog/src/templates/blog-post.js
+++ services/blog/src/templates/blog-post.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
-const BlogPostTemplate = ({data}) => {
+const BlogPostTemplate = ({data, pageContext}) => {
+    const {next, prev} = pageContext
     const { markdownRemark } = data
     const title = markdownRemark.frontmatter.title
     const html = markdownRemark.html
@@ -12,11 +13,29 @@ const BlogPostTemplate = ({data}) => {
                 className='blogpost'
                 dangerouslySetInnerHTML={{__html: html}}
             />
+            <div style={{marginBottom:'1rem'}}>
+                {prev && 
+                    <Link to={prev.frontmatter.path}>
+                        Prev
+                    </Link>
+                }
+            </div>
+            <div style={{marginBottom:'1rem'}}>
+                {next && 
+                    <Link to={next.frontmatter.path}>
+                        Next
+                    </Link>
+                }
+            </div>
+            <div>
+            <Link to='/'>Home</Link>
+            </div>
         </div>
     )
 }
 
-export const query = graphql `
+export const query = graphql 
+`
     query($pathSlug: String!) {
         markdownRemark(frontmatter: { path: {eq: $pathSlug} }) {
             html
